5967fb6207b07131002a873d1dc793d3359ac9ac
adntayer <29784844+adntayer@users.noreply.github.com>
2025-04-05 22:30:48 -0300

    first load

---
+++ cartola_datalake/mesh/checksum.py
@@ -0,0 +1,10 @@
+import hashlib
+import json
+
+
+def calculate_dict_checksum(data):
+    data_str = json.dumps(data, sort_keys=True)
+
+    checksum = hashlib.sha256(data_str.encode('utf-8')).hexdigest()
+
+    return checksum
